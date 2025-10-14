2bf882bdf9324e70c635188f2fd85604c9d94413
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-10-14 18:36:44 -0300

    commit para probar pre-push

--- src/test/java/com/ingsis/lintSnippetService/FormatAndLintingServiceApplicationTests.java
+++ src/test/java/com/ingsis/lintSnippetService/FormatAndLintingServiceApplicationTests.java
@@ -1,9 +1,9 @@
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FormatAndLintingServiceApplicationTests {
-
-  @Test
-  void contextLoads() {}
-}
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+//
+// @SpringBootTest
+// class FormatAndLintingServiceApplicationTests {
+//
+//  @Test
+//  void contextLoads() {}
+// }
