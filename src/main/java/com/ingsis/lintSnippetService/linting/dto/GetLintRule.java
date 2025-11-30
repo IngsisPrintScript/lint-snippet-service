3ab2adec95540b9093debeaedea1c8e31b91477b
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-11-30 19:37:44 -0300

    spotless

--- src/main/java/com/ingsis/lintSnippetService/linting/dto/GetLintRule.java
+++ src/main/java/com/ingsis/lintSnippetService/linting/dto/GetLintRule.java
@@ -1,7 +1,5 @@
 package com.ingsis.lintSnippetService.linting.dto;
 
-import java.io.Serializable;
 import java.util.UUID;
 
-public record GetLintRule(UUID lintId, String name, boolean active){
-}
+public record GetLintRule(UUID lintId, String name, boolean active) {}
